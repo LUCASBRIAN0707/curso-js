4123cc3458fc94fc796ac5717c53246a0305a727
LUCAS BRIAN <briandeoliveiralucas@gmail.com>
2026-03-09 13:54:53 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { // condição simples!
+    console.log(`você ultrapassou a velocidade permitida. multado!`)
+}
+console.log(`dirige sempre usando cinto de segurança!`)
