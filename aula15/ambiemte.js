84de24786c2b1ca4251df90247c0a372fd781d4a
LUCAS BRIAN <briandeoliveiralucas@gmail.com>
2026-03-20 16:52:03 -0300

    Add files via upload

---
+++ aula15/ambiemte.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`o primeiro valor é ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log(`o valor não foi encontrado!`)
+} else {
+    console.log(`o valor está na posição ${pos}`)
+}
